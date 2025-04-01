83e187d8a6e3cb4dac285e701b40947baba94f8b
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 21:24:25 +0300

    bugs fixed

--- sorted_insert.py
+++ sorted_insert.py
@@ -8,19 +8,19 @@ class Node(object):
 
 
 def sorted_insert(head, data):
-    if not head:
-        return
+    if head is None:
+        return Node(data)
     if data <= head.data:
         result = Node(data)
         result.next = head
         return result
     current_node = head
     while current_node.next is not None:
-        if current_node.next.data < data:
+        if current_node.next.data > data:
             break
         current_node = current_node.next
     temp = current_node.next
     temp_node = Node(data)
     temp_node.next = temp
     current_node.next = temp_node
-    return current_node
+    return head
