aabc66b4134ad4eeb2105204c3e8e69708abde65
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 10:34:19 +0300

    update class Node, made parse function

--- linked_list_class.py
+++ linked_list_class.py
@@ -5,3 +5,6 @@ class Node():
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
+    
+    def __str__(self):
+        return f'Node({self.data}, {str(self.next)})'
--- parse_a_linked_list_from_a_string.py
+++ parse_a_linked_list_from_a_string.py
@@ -6,4 +6,9 @@ from linked_list_class import Node
 
 def linked_list_from_string(s):
     """Make a linked list from string"""
-    return # head to linked list
+    order = s.split(' -> ')
+    if order[1] == 'None':
+        return Node(int(order[0]))
+    return Node(order[0], linked_list_from_string(' -> '.join(order[1:])))
+
+print(linked_list_from_string("1 -> 2 -> 3 -> None"))
