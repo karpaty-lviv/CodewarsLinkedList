6847b8ed68ed5e04d81c52e2bdcce83a85169b8f
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 17:53:13 +0300

    bug fixed

--- get_nth_node.py
+++ get_nth_node.py
@@ -5,8 +5,14 @@ from linked_list_class import Node
 
 
 def get_nth(node, index):
+    """Get nth node"""
     result = node
+    if index < 0:
+        raise Exception
+    if result is None:
+            raise Exception
     for _ in range(index):
         result = result.next
+        if result is None:
+            raise Exception
     return result
-
