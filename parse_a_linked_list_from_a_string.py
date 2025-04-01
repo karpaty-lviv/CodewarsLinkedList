432a6a806fe580611c85aa5cee57039104bdfd96
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 10:39:41 +0300

    bug fixed

--- parse_a_linked_list_from_a_string.py
+++ parse_a_linked_list_from_a_string.py
@@ -7,8 +7,26 @@ from linked_list_class import Node
 def linked_list_from_string(s):
     """Make a linked list from string"""
     order = s.split(' -> ')
+    if len(order) == 1:
+        return Node(order[0])
     if order[1] == 'None':
         return Node(int(order[0]))
     return Node(order[0], linked_list_from_string(' -> '.join(order[1:])))
 
-print(linked_list_from_string("1 -> 2 -> 3 -> None"))
+
+def stringify(node):
+    """Returns string represantation of linked list"""
+    if node is None:
+        return 'None'
+    text = f'{node.data}'
+    if node.next is None:
+        return f'{text} -> None'
+    current_node = node.next
+    while current_node.next is not None:
+        text += f' -> {current_node.data}'
+        current_node = current_node.next
+    text += f' -> {current_node.data} -> None'
+    return text
+
+
+print(stringify(linked_list_from_string("1 -> 2 -> 3 -> None")))
