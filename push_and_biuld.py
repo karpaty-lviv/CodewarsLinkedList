32e3ce3668939d750fa5548e51e9cbd7bb3f8fb4
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 13:31:51 +0300

    make file push_and_build

---
+++ push_and_biuld.py
@@ -0,0 +1,13 @@
+"""Push & BuildOneTwoThree"""
+
+
+from linked_list_class import Node
+
+
+def push(head, data):
+    """Push"""
+    return Node(None)
+
+def build_one_two_three():
+    """1, 2, 3"""
+    return Node(1, Node(2, Node(3)))
