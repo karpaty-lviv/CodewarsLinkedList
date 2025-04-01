a33810e89dadf69e4c753f2aea0eb76d958598eb
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 23:35:54 +0300

    better pylint

--- recursive_reverse.py
+++ recursive_reverse.py
@@ -2,11 +2,13 @@
 
 
 class Node(object):
+    """Linked list"""
     def __init__(self, data=None):
         self.data = data
         self.next = None
 
 def reverse(head, prev=None):
+    """Reverse"""
     if head is None:
         return prev
     next_node = head.next
