b5caec4e5a456ec8fca128e6ec8fb45752c0c0d7
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 18:13:21 +0300

    made fist version

---
+++ move_node.py
@@ -0,0 +1,22 @@
+"""Move node"""
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    """Move node"""
+    
+    first_node = source.data
+    source = source.next
+    new_dest = Node(first_node)
+    new_dest.next = dest
+    return Context(source, new_dest)
