f23e0939d64380269a3e0c1ff74ab788a4ea2306
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-01 10:18:27 +0300

    create file

---
+++ parse_a_linked_list_from_a_string.py
@@ -0,0 +1,9 @@
+"""Parse a linked list from a string"""
+
+
+from linked_list_class import Node
+
+
+def linked_list_from_string(s):
+    """Make a linked list from string"""
+    return # head to linked list
