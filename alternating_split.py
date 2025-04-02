9239a5a9647bb3635d33ec207209ae99b274495e
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-02 10:32:11 +0300

    made new files

---
+++ alternating_split.py
@@ -0,0 +1,18 @@
+"""Alternating Split"""
+
+
+class Node(object):
+    """Linked List"""
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    """Context"""
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+def alternating_split(head):
+    """Split"""
+    pass
---
+++ can_you_get_the_loop.py
@@ -0,0 +1,13 @@
+"""Can you get the loop ?"""
+
+
+class Node():
+    """Class for node in linked list"""
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+
+def loop_size(node):
+    """Find loop size"""
+    pass
