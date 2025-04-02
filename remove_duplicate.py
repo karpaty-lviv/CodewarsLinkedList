c43675460bf8959663740faf8d902ba7cc38a6ea
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-02 09:04:09 +0300

    made file remove duplicates

---
+++ remove_duplicate.py
@@ -0,0 +1,27 @@
+"""Remove Duplicate"""
+
+
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return f'Node({self.data}, {str(self.next)})'
+
+def remove_duplicates(head):
+    """Remove duplicate"""
+    if head is None:
+        return None
+    current_node = head
+    while current_node.next is not None:
+        print(current_node.data)
+        print(current_node.next.data)
+        if current_node.data == current_node.next.data:
+            current_node = current_node.next
+            current_node.next = current_node.next.next
+        else:
+            current_node = current_node.next
+    return head
+
+print(remove_duplicates(Node(1, Node(2, Node(2, Node(3, Node(3, Node(4, Node(5)))))))))
