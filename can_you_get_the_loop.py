0fd7c3905e2eeb361a9cbf6d28373c4ab16f4301
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-02 16:34:52 +0300

    make function

--- can_you_get_the_loop.py
+++ can_you_get_the_loop.py
@@ -10,4 +10,13 @@ class Node():
 
 def loop_size(node):
     """Find loop size"""
-    pass
+    turtle = node
+    rabit = node.next.next
+    while turtle != rabit:
+        turtle = turtle.next
+        rabit = rabit.next.next
+    counter = 1
+    rabit = rabit.next
+    while rabit != turtle:
+        rabit = rabit.next
+    return counter
