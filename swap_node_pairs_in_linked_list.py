bf4c7aa83aa0223cab3f9863239a4a27f7676544
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-02 09:39:11 +0300

    made file swap node

---
+++ swap_node_pairs_in_linked_list.py
@@ -0,0 +1,14 @@
+"""Swap Node Pairs In Linked List"""
+
+
+class Node:
+    """Class for linked list"""
+    def __init__(self, next=None):
+        self.next = next
+
+
+def swap_pairs(head):
+    """Swap pairs"""
+    if head is None:
+        return None
+    return head
